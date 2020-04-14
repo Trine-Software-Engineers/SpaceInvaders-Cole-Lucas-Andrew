d79e48ff06d0c1c084434516d21a89896055f951
Lucas Wagler <lucas@wagler.dev>
2020-04-14 07:39:31 -0400

    Add Leaderboard to menu

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,7 @@
 #include "menu.h"
 #include "ui_menu.h"
 #include <QHBoxLayout>
+#include "leaderboard.h"
 
 Menu::Menu(QWidget *parent) :
     QMainWindow(parent),
@@ -23,4 +24,11 @@ void Menu::on_pushButton_clicked()
 {
     this->hide();
     Game.show();
+}
+
+void Menu::on_pushButton_Leaderboard_clicked()
+{
+    leaderboard board;
+//    board.setScore(0);
+    board.showLeaderboard();
 }
--- menu.h
+++ menu.h
@@ -21,6 +21,8 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_Leaderboard_clicked();
+
 private:
     Ui::Menu *ui;
     MainWindow Game;
